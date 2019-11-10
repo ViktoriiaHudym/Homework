3cfc291bb55e51408b617d3e8926906ccd317bb0
ViktoriiaHudym <vikahudym222@gmail.com>
2019-11-10 21:03:44 +0200

    task 3

--- Lab 1/task3.py
+++ Lab 1/task3.py
@@ -1,13 +1,18 @@
 import re
 def function(x):
     return bool(re.match('^[-+]{0,1}\d+$', x))
+
 def function_input():
-    x = input('Enter x: ')
+    x = input('Введіть змінну x: ')
     while not function(x):
-        x = input('Enter x again: ')
+        x = input('Помилка! Введіть x знову: ')
+    return main(x)
+
+def main(x):
     x = int(x)
     if x<=7:
         return(3*x-9)
     else:
         return(1/(x**2-4))
-print('F(x)=', function_input())
+
+print('F(x)=',function_input())
